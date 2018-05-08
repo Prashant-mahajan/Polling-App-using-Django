397fe5f203a56451d80743b83b24d566c132ffd2
Prashant-mahajan <prashantmahajan70@gmail.com>
2018-05-08 20:15:45 +0530

    Issue with CSS resolved

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -118,10 +118,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
-    # '/mysite/polls',
-    'Users/prashant/Downloads/Projects/Polling-App-Using-Django/mysite/polls/static'
-]
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, "static"),
+#     # '/mysite/polls',
+#     'Users/prashant/Downloads/Projects/Polling-App-Using-Django/mysite/polls/static'
+# ]
 
 STATIC_URL = '/static/'
