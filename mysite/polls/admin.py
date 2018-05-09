655903cf8b8041a5635ae8787073aed04aeb9ec6
Prashant-mahajan <prashantmahajan70@gmail.com>
2018-05-09 13:27:48 +0530

    Made changes in CSS files and customisation of Admin page

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -1,6 +1,18 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Question
+from .models import Choice, Question
 
-admin.site.register(Question)
+class ChoiceInline(admin.TabularInline):
+	model = Choice
+	extra = 3
+
+class QuestionAdmin(admin.ModelAdmin):
+	fieldsets = [
+		(None, 				 {'fields' : ['question_text']}),
+		('Date Information', {'fields' : ['pub_date']})
+	]
+	inlines = [ChoiceInline]
+
+
+admin.site.register(Question, QuestionAdmin)
